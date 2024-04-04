cda40d39f7dca3376b22dd8a3885abf3472baaeb
Migueleonfer27 <miguellefer@gmail.com>
2024-04-04 23:53:44 +0200

    paquete de inputs

---
+++ src/main/kotlin/menusBackend/InputsLogin.kt
@@ -0,0 +1,40 @@
+package menusBackend
+
+import usuario.Usuario
+
+class InputsLogin {
+
+    /**
+     * Permite al usuario ingresar un correo electrónico y verifica si coincide con el correo electrónico del usuario proporcionado.
+     *
+     * @param usuario el objeto Usuario que contiene el correo electrónico.
+     */
+    fun ingresoEmail(usuario: Usuario) {
+        var emailInput: String?
+
+        do {
+            print("Ingrese el email: ")
+            emailInput = readlnOrNull()
+            if (usuario.email != emailInput) {
+                println("!Email incorrecto!")
+            }
+        } while (usuario.email != emailInput)
+    }
+
+    /**
+     * Permite al usuario ingresar una contraseña y verifica si coincide con la contraseña del usuario proporcionado.
+     *
+     * @param usuario El objeto Usuario que contiene la contraseña.
+     */
+    fun ingresoContrasenia(usuario: Usuario) {
+        var contrasenia: String?
+
+        do {
+            print("Introducir contraseña: ")
+            contrasenia = readlnOrNull()
+            if (contrasenia != usuario.contrasenia) {
+                print("Por favor, introduce la contraseña correctamente: ")
+            }
+        } while (contrasenia != usuario.contrasenia)
+    }
+}
---
+++ src/main/kotlin/menusBackend/InputsRegistro.kt
@@ -0,0 +1,101 @@
+package menusBackend
+
+class InputsRegistro {
+
+    /**
+     * Solicita al usuario que introduzca su nombre y devuelve este nombre como una cadena de texto.
+     *
+     * Esta función garantiza que el nombre ingresado no sea nulo y solicita al usuario que lo vuelva a ingresar en caso de ser nulo.
+     *
+     * @return El nombre ingresado por el usuario como una cadena de texto.
+     */
+    fun introducirNombre(): String {
+        var nombre: String?
+
+        do {
+            print("Introduce tu nombre: ")
+            nombre = readlnOrNull()
+            if (nombre == null) {
+                println("!ERROR!")
+            }
+        } while (nombre == null)
+
+        return nombre
+    }
+
+    /**
+     * Solicita al usuario que introduzca un correo electrónico y devuelve el correo electrónico como una cadena de texto.
+     *
+     * @return El correo electrónico ingresado por el usuario como una cadena de texto.
+     */
+    fun introducirEmail(): String {
+        var email: String?
+
+        do {
+            print("Introduce un email: ")
+            email = readlnOrNull()
+            if (email == null) {
+                println("!ERROR!")
+            }
+        } while (email == null)
+
+        return email
+    }
+
+    /**
+     * Solicita al usuario que introduzca sus apellidos y devuelve los apellidos como una cadena de texto.
+     *
+     * @return Los apellidos ingresados por el usuario como una cadena de texto.
+     */
+    fun introducirApellidos(): String {
+        var apellido: String?
+
+        do {
+            print("Introduce tus apellidos: ")
+            apellido = readlnOrNull()
+            if (apellido == null) {
+                println("!ERROR!")
+            }
+        } while (apellido == null)
+
+        return apellido
+    }
+
+    /**
+     * Solicita al usuario que introduzca su edad y devuelve la edad como un número entero.
+     *
+     * @return La edad ingresada por el usuario como un número entero.
+     */
+    fun introducirEdad(): Int {
+        var edad: Int?
+
+        do {
+            print("Introduce tu edad: ")
+            edad = readln().toIntOrNull()
+            if (edad == null || edad < 0 || edad > 150) {
+                println("!ERROR!")
+            }
+        } while (edad == null || edad < 0 || edad > 150)
+
+        return edad
+    }
+
+    /**
+     * Solicita al usuario que introduzca una contraseña de mínimo 5 cararacteres y devuelve la contraseña como una cadena de texto.
+     *
+     * @return La contraseña ingresada por el usuario como una cadena de texto.
+     */
+    fun introducirContrasenia(): String {
+        var contrasenia: String?
+
+        do {
+            print("Contraseña [5 dígitos mínimos]: ")
+            contrasenia = readlnOrNull()
+            if (contrasenia == null || contrasenia.length < 5) {
+                println("!ERROR!")
+            }
+        } while (contrasenia == null || contrasenia.length < 5)
+
+        return contrasenia
+    }
+}
