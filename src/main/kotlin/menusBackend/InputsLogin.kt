ab27aafce2ad51f295569ff2096b31d22f46eae5
Carlos <carlos.canal.da.silva@gmail.com>
2024-04-05 11:14:24 +0200

    Creada la clase de Login

--- src/main/kotlin/menusBackend/InputsLogin.kt
+++ src/main/kotlin/menusBackend/InputsLogin.kt
@@ -3,34 +3,36 @@ package menusBackend
 import usuario.Usuario
 
 class InputsLogin {
-    /**
-     * Permite al usuario ingresar un correo electrónico y verifica si coincide con el correo electrónico del usuario proporcionado.
-     */
-    fun ingresoEmail(): String {
-        var emailInput: String?
+    companion object {
+        /**
+         * Permite al usuario ingresar un correo electrónico y verifica si coincide con el correo electrónico del usuario proporcionado.
+         */
+        fun ingresoEmail(): String {
+            var emailInput: String?
 
-        do {
-            print("Ingrese el email: ")
-            emailInput = readlnOrNull()
-        } while (emailInput == null)
+            do {
+                print("Ingrese el email: ")
+                emailInput = readlnOrNull()
+            } while (emailInput == null)
 
-        return emailInput
-    }
+            return emailInput
+        }
 
-    /**
-     * Permite al usuario ingresar una contraseña y verifica si coincide con la contraseña del usuario proporcionado.
-     *
-     * @param usuario El objeto Usuario que contiene la contraseña.
-     */
-    fun ingresoContrasenia(usuario: Usuario) {
-        var contrasenia: String?
+        /**
+         * Permite al usuario ingresar una contraseña y verifica si coincide con la contraseña del usuario proporcionado.
+         *
+         * @param usuario El objeto Usuario que contiene la contraseña.
+         */
+        fun ingresoContrasenia(usuario: Usuario) : Boolean {
+            var contrasenia: String?
 
-        do {
-            print("Introducir contraseña: ")
-            contrasenia = readlnOrNull()
-            if (contrasenia != usuario.contrasenia) {
-                print("Por favor, introduce la contraseña correctamente: ")
-            }
-        } while (contrasenia != usuario.contrasenia)
+            do {
+                print("Introducir contraseña: ")
+                contrasenia = readlnOrNull()
+                if (contrasenia != usuario.contrasenia) {
+                    print("Por favor, introduce la contraseña correctamente: ")
+                }
+            } while (contrasenia != usuario.contrasenia)
+        }
     }
 }
--- src/main/kotlin/menusBackend/InputsMenus.kt
+++ src/main/kotlin/menusBackend/InputsMenus.kt
@@ -1,34 +1,36 @@
 package menusBackend
 
 class InputsMenus {
-    /**
-     * Solicita al usuario que introduzca un número que represente una opción del menú,
-     * asegurándose de que el número esté dentro del rango [1 - numOpciones].
-     *
-     * @param numeroOpciones El número total de opciones disponibles en el menú.
-     * @return El número de la opción seleccionada por el usuario.
-     */
+    companion object {
+        /**
+         * Solicita al usuario que introduzca un número que represente una opción del menú,
+         * asegurándose de que el número esté dentro del rango [1 - numOpciones].
+         *
+         * @param numeroOpciones El número total de opciones disponibles en el menú.
+         * @return El número de la opción seleccionada por el usuario.
+         */
 
-    fun seleccionarOpcionMenu(numeroOpciones: Int): Int {
-        var opt: Int?
+        fun seleccionarOpcionMenu(numeroOpciones: Int): Int {
+            var opt: Int?
 
-        do {
-            print("Introduce una opción [1-$numeroOpciones]: ")
-            opt = readln().toIntOrNull()
-            if (opt == null || opt < 1 || opt > numeroOpciones) {
-                println("!ERROR!")
-            }
-        } while (opt == null || opt < 1 || opt > numeroOpciones)
+            do {
+                print("Introduce una opción [1-$numeroOpciones]: ")
+                opt = readln().toIntOrNull()
+                if (opt == null || opt < 1 || opt > numeroOpciones) {
+                    println("!ERROR!")
+                }
+            } while (opt == null || opt < 1 || opt > numeroOpciones)
 
-        return opt
-    }
+            return opt
+        }
 
-    /**
-     * Indica la acción de salida del menú.
-     *
-     * @return `true` para indicar que se debe salir del menú.
-     */
-    fun salirMenu(): Boolean {
-        return true
+        /**
+         * Indica la acción de salida del menú.
+         *
+         * @return `true` para indicar que se debe salir del menú.
+         */
+        fun salirMenu(): Boolean {
+            return true
+        }
     }
 }
