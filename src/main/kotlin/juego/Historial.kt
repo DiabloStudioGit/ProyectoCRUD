23cbe994a309c4137f22a0f996a8a9ba29214de8
Siko <kayserhack@gmail.com>
2024-04-08 04:54:17 +0200

    Añadido Serializable Historial

--- src/main/kotlin/juego/Historial.kt
+++ src/main/kotlin/juego/Historial.kt
@@ -1,6 +1,7 @@
 package juego
 
-data class Historial(var emailJugador : String, var partidasJugadas : Int, var partidasGanadas : Int, var puntos : Int) {
+import java.io.Serializable
+data class Historial(var emailJugador : String, var partidasJugadas : Int, var partidasGanadas : Int, var puntos : Int): Serializable {
     override fun toString(): String {
         return "partida:   \n  Email: $emailJugador \n  Partidas Jugadas: $partidasJugadas \n  Partidas Ganadas: $partidasGanadas \n  Puntos: $puntos"
     }
